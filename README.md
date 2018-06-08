3d5126a21a8c3e1d90190a7c518b72c09834fa5f
fengrui <frvip@live.cn>
2018-06-08 10:25:05 +0800

    1

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # anydoor
 Tiny Node JS statice Web server
+
+## 使用方法
---
+++ index.js
@@ -0,0 +1,7 @@
+
+console.log(123);
+console.log(123);// eslint-disable-line
+
+// eslint-disable-next-line
+console.log(123);
+
